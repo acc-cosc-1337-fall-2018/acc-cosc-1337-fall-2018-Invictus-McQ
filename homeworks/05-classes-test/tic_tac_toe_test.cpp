485cf3fdf6880e048f8400319c8031dea1cbc329
Kevin McCue Home <mcckm.kvn@gmail.com>
2018-10-06 13:37:32 -0500

    HW5 done.
    Some weird issue with the TEST cases not working... probably can't use them here?

--- homeworks/05-classes-test/tic_tac_toe_test.cpp
+++ homeworks/05-classes-test/tic_tac_toe_test.cpp
@@ -1,4 +1,154 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "tic_tac_toe_board.h"
+#include <string>
+#include <vector>
+#include <iostream>
 
+TEST_CASE("test win by first column", "[x wins in column 1]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.mark_board(0); //x
+	board.mark_board(2);//o
+	board.mark_board(3);//x
+	board.mark_board(5);//o
+	board.mark_board(6);//x
+	// x wins
+
+	REQUIRE(board.game_over() == true);
+
+}
+
+TEST_CASE("test win by second column", "[x wins in column 2]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.mark_board(1);//x
+	board.mark_board(3); //o
+	board.mark_board(4);//x
+	board.mark_board(5);//o
+	board.mark_board(7);//x
+	// x wins
+
+	REQUIRE(board.game_over() == true);
+
+}
+
+TEST_CASE("test win by third column", "x wins in column 3]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.mark_board(2);//x
+	board.mark_board(3); //o
+	board.mark_board(5);//x
+	board.mark_board(6);//o
+	board.mark_board(8);//x
+	// x wins
+
+	REQUIRE(board.game_over() == true);
+
+}
+
+TEST_CASE("test win by first row", "[x wins in row 1]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.mark_board(0);//x
+	board.mark_board(3); //o
+	board.mark_board(1);//x
+	board.mark_board(5);//o
+	board.mark_board(2);//x
+	// x wins
+
+	REQUIRE(board.game_over() == true);
+
+}
+
+TEST_CASE("test win by secon row", "[x wins in row 2]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.mark_board(3);//x
+	board.mark_board(1); //o
+	board.mark_board(4);//x
+	board.mark_board(7);//o
+	board.mark_board(5);//x
+	// x wins
+
+	REQUIRE(board.game_over() == true);
+
+}
+
+TEST_CASE("test win by third row", "[x wins in row 3]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.mark_board(6);//x
+	board.mark_board(3); //o
+	board.mark_board(7);//x
+	board.mark_board(5);//o
+	board.mark_board(8);//x
+	// x wins
+
+	REQUIRE(board.game_over() == true);
+
+}
+
+TEST_CASE("test win diagonally pegs 0, 4, 8", "[x wins in diag 1]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.mark_board(0);//x
+	board.mark_board(3); //o
+	board.mark_board(4);//x
+	board.mark_board(5);//o
+	board.mark_board(8);//x
+	// x wins
+
+	REQUIRE(board.game_over() == true);
+
+}
+
+TEST_CASE("test win diagonally pegs 2, 4, 6", "[x wins in diag 2]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.mark_board(2);//x
+	board.mark_board(3); //o
+	board.mark_board(4);//x
+	board.mark_board(0);//o
+	board.mark_board(6);//x
+	// x wins
+
+	REQUIRE(board.game_over() == true);
+
+}
+
+TEST_CASE("test no winner", "[no win]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.mark_board(0);//x
+	board.mark_board(1); //o
+	board.mark_board(2);//x
+	board.mark_board(3);//o
+	board.mark_board(5);//x
+	board.mark_board(4); //o
+	board.mark_board(6);//x
+	board.mark_board(8);//o
+	board.mark_board(7);//x
+	// no one wins
+
+	REQUIRE(board.game_over() == true);
+
+}
+
+TEST_CASE("Test first player is X", "[returns X]")
+{
+	TicTacToeBoard board;
+	board.start_game("X");
+	board.get_player();
+
+	REQUIRE(board.get_player() == "X");
+}
--- homeworks/05-classes/main.cpp
+++ homeworks/05-classes/main.cpp
@@ -1,9 +1,51 @@
 #include "tic_tac_toe_board.h"
 
 
+using std::endl; using std::cout; using std::cin; using std::string;
+
 
 int main()
 {
-	
+
+	string play_again = "y";
+	int peg_pos = 0;
+	bool endgame = false;
+	string winner;
+	cout << "It's Kevin's super simple Tic-Tac-Toe! Have fun! OR ELSE!" << endl;
+
+	TicTacToeBoard game;
+	game.start_game("X");
+	game.display_board();
+	while (play_again != "q")
+	{
+
+		cout << "These are the peg positions on the board (1 to 9)" << endl;
+		cout << "[1]" << "[2]" << "[3]" << endl << "[4]" << "[5]" << "[6]" << endl << "[7]" << "[8]" << "[9]" << endl;
+
+		while (game.game_over() == false) {
+			std::cout << "Enter the position for " << game.get_player() << ":  ";
+			std::cin >> peg_pos;
+			game.mark_board(peg_pos);
+			std::cout << std::endl;
+			game.display_board();
+		}
+
+		if (game.get_player() == "X") 
+		{
+			winner = "O";
+		}
+		else winner = "X";
+
+		cout << "GAME OVER" << endl << endl << endl;
+		std::cout << "Congratulations! " << winner << " wins!" << std::endl;
+		cout << "Would you like to play again? " << endl;
+		cout << "If yes, PRESS ENTER!" << endl << endl;
+		cout << "Otherwise, press 'q' then ENTER to quit." << endl;
+		cin >> play_again;
+
+	}
 	return 0;
 }
+
+
+	
